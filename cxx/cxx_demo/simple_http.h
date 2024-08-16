a4c49e2ee26748f6eb9f849e7cbd6b37bd1283e1
AeroNyx <130458686+AeroNyxNetwork@users.noreply.github.com>
2024-08-17 02:11:23 +0800

    Update simple_http.h

--- cxx/cxx_demo/simple_http.h
+++ cxx/cxx_demo/simple_http.h
@@ -1,5 +1,6 @@
 #ifndef __SIMPLE_HTTP__H__
 #define __SIMPLE_HTTP__H__
+
 #include <iostream>
 #include <boost/asio.hpp>
 #include <boost/asio/ssl.hpp>
@@ -8,34 +9,47 @@
 #include <boost/beast/version.hpp>
 #include <boost/beast/http/string_body.hpp>
 
+// Function to perform an HTTP GET request
 std::string HttpGet(const std::string& host, const std::string& port, const std::string& target) {
-    std::string rtn = "";
+    std::string rtn = "";  // Variable to store the response body as a string
     try {
-        boost::asio::io_context ioc;
-        boost::asio::ip::tcp::resolver resolver(ioc);
-        boost::asio::ip::tcp::socket socket(ioc);
+        boost::asio::io_context ioc;  // Context for managing objects requiring I/O
+        boost::asio::ip::tcp::resolver resolver(ioc);  // Resolver to turn the server name into a TCP endpoint
+        boost::asio::ip::tcp::socket socket(ioc);  // Socket for the connection
 
+        // Resolve the host name and service to a list of endpoints
         auto const results = resolver.resolve(host, port);
 
+        // Establish a connection to the first endpoint in the list of resolved endpoints
         boost::asio::connect(socket, results.begin(), results.end());
+
+        // Create an HTTP GET request message
         boost::beast::http::request<boost::beast::http::string_body> req{boost::beast::http::verb::get, target, 11};
-        req.set(boost::beast::http::field::host, host);
-        req.set(boost::beast::http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+        req.set(boost::beast::http::field::host, host);  // Set the Host HTTP header
+        req.set(boost::beast::http::field::user_agent, BOOST_BEAST_VERSION_STRING);  // Set the User-Agent HTTP header
+
+        // Send the HTTP request to the remote host
         boost::beast::http::write(socket, req);
-        boost::beast::flat_buffer buffer;
+
+        // This buffer is used for reading and must be persisted
+        boost::beast::flat_buffer buffer;  
+        // Declare a container to hold the response
         boost::beast::http::response<boost::beast::http::dynamic_body> res;
+
+        // Receive the HTTP response
         boost::beast::http::read(socket, buffer, res);
 
+        // Convert the response body to a string
         std::string body = boost::beast::buffers_to_string(res.body().data());
-
         rtn = body;
 
+        // Gracefully close the socket
         boost::beast::error_code ec;
         socket.shutdown(boost::asio::ip::tcp::socket::shutdown_both, ec);
     } catch (std::exception const& e) {
-        std::cout << "Faild:" << e.what() << std::endl;
+        std::cout << "Failed: " << e.what() << std::endl;  // Output error message if an exception is thrown
     }
     return rtn;
 }
 
-#endif
+#endif  // __SIMPLE_HTTP__H__
